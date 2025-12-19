43e8d00c8915ffb8e23269b9bd510adde5c2cde6
VaibhavShukla2004 <vaibhavshukla1110r@gmail.com>
2025-12-20 01:49:21 +0530

    Controller Production Ready

--- src/main/java/com/email/writer/app/Controller/EmailGeneratorController.java
+++ src/main/java/com/email/writer/app/Controller/EmailGeneratorController.java
@@ -1,6 +1,5 @@
 package com.email.writer.app.Controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.email.writer.app.DTO.EmailRequest;
 import com.email.writer.app.Service.EmailGeneratorService;
-import com.google.genai.Client;
-import com.google.genai.types.GenerateContentResponse;
-
 import lombok.AllArgsConstructor;
 import reactor.core.publisher.Mono;
 
@@ -26,20 +22,4 @@ public class EmailGeneratorController {
         return emailGeneratorService.generateEmailReply(emailRequest);
     }
 
-    @PostMapping("/test-gemini")
-    public ResponseEntity<String> testGemini() {
-
-        Client client = Client.builder()
-                .apiKey(System.getenv("GEMINI_KEY"))
-                .build();
-        
-        System.out.println("Gemini Key = " + System.getenv("GEMINI_KEY"));
-
-        GenerateContentResponse response = client.models.generateContent(
-                "gemini-2.5-flash",
-                "Explain how AI works in a few words",
-                null);
-
-        return ResponseEntity.ok(response.text());
-    }
 }
