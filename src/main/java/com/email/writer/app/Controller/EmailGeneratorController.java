bf5d8f352c491ddf587211be72d3f06614cc2ad9
VaibhavShukla2004 <vaibhavshukla1110r@gmail.com>
2025-12-20 01:42:33 +0530

    Controller and Service working

---
+++ src/main/java/com/email/writer/app/Controller/EmailGeneratorController.java
@@ -0,0 +1,46 @@
+package com.email.writer.app.Controller;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.email.writer.app.DTO.EmailRequest;
+import com.email.writer.app.Service.EmailGeneratorService;
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
+
+import lombok.AllArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/email")
+@AllArgsConstructor
+public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
+    public Mono<String> generateEmail(@RequestBody EmailRequest emailRequest) {
+        return emailGeneratorService.generateEmailReply(emailRequest);
+    }
+
+    @PostMapping("/test-gemini")
+    public ResponseEntity<String> testGemini() {
+
+        Client client = Client.builder()
+                .apiKey(System.getenv("GEMINI_KEY"))
+                .build();
+        
+        System.out.println("Gemini Key = " + System.getenv("GEMINI_KEY"));
+
+        GenerateContentResponse response = client.models.generateContent(
+                "gemini-2.5-flash",
+                "Explain how AI works in a few words",
+                null);
+
+        return ResponseEntity.ok(response.text());
+    }
+}
---
+++ src/main/java/com/email/writer/app/Service/EmailGeneratorService.java
@@ -0,0 +1,72 @@
+package com.email.writer.app.Service;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.email.writer.app.DTO.EmailRequest;
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
+
+import reactor.core.publisher.Mono;
+
+@Service
+public class EmailGeneratorService {
+
+    private final WebClient webClient;
+
+    public EmailGeneratorService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public Mono<@Nullable String> generateEmailReply(EmailRequest emailRequest) {
+
+        // Build the prompt
+        String prompt = buildPrompt(emailRequest);
+
+        // verify environment variables
+        System.out.println("URL = " + geminiApiUrl);
+        System.out.println("KEY = " + geminiApiKey);
+
+        // build Gemini client
+        Client client = Client.builder()
+                .apiKey(System.getenv("GEMINI_KEY"))
+                .build();
+
+        //get Response from Gemini
+        GenerateContentResponse response = client.models.generateContent(
+                "gemini-2.5-flash",
+                prompt,
+                null);                                       
+
+        // return reply from Gemini
+        return Mono.just(response.text());
+    }
+
+    public Mono<String> extractText(GenerateContentResponse response) {
+        String content = response.text();
+        return Mono.just(content);
+    }
+
+    private String buildPrompt(EmailRequest emailRequest) {
+
+        StringBuilder prompt = new StringBuilder();
+        prompt.append(
+                "Generate a professional email reply for the following email content. Please don't generate a subject line :\n");
+        if (emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()) {
+            prompt.append("Tone: ").append(emailRequest.getTone()).append("\n");
+        }
+
+        prompt.append("Original email: ").append(emailRequest.getEmailContent()).append("\n");
+
+        return prompt.toString();
+    }
+}
