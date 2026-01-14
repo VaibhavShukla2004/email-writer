fd3762a0da8d05fb3da578d97d4b8b202cdfb120
VaibhavShukla2004 <vaibhavshukla1110r@gmail.com>
2026-01-15 01:05:13 +0530

    Lock down CORS to frontend URL after successful integration test

--- src/main/java/com/email/writer/app/Controller/EmailGeneratorController.java
+++ src/main/java/com/email/writer/app/Controller/EmailGeneratorController.java
@@ -14,7 +14,7 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "{FRONTEND_URL}")
 public class EmailGeneratorController {
 
     private final EmailGeneratorService emailGeneratorService;
